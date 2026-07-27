4127ba2febbed0e53e551c8180a9f604cb528046
Raja Pavan Tarun Kumar Kopparthi <kopparthiraja52@gmail.com>
2026-07-27 21:40:06 +0530

    Extend SpringBootServletInitializer and add provided Tomcat dependency for WAR deployment

--- adaptive_course_backend/src/main/java/com/example/demo/AdaptiveCourseBackendApplication.java
+++ adaptive_course_backend/src/main/java/com/example/demo/AdaptiveCourseBackendApplication.java
@@ -2,9 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AdaptiveCourseBackendApplication {
+public class AdaptiveCourseBackendApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AdaptiveCourseBackendApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AdaptiveCourseBackendApplication.class, args);
