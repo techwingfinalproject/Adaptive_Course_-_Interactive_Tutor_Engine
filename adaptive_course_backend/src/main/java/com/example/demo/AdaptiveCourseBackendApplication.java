17bbdb1945549af2bd191ac5d667783f47563a68
seshu7337 <seshutallapu@gmail.com>
2026-07-29 00:10:34 +0530

    Extend AdaptiveCourseBackendApplication for servlet support
    Extended AdaptiveCourseBackendApplication to support servlet initialization and added configuration method.

--- adaptive_course_backend/src/main/java/com/example/demo/AdaptiveCourseBackendApplication.java
+++ adaptive_course_backend/src/main/java/com/example/demo/AdaptiveCourseBackendApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder; // 1. add
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer; // 2. add
 
 @SpringBootApplication
-public class AdaptiveCourseBackendApplication {
+public class AdaptiveCourseBackendApplication extends SpringBootServletInitializer { // 3. extend
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) { // 4. add this method
+        return application.sources(AdaptiveCourseBackendApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AdaptiveCourseBackendApplication.class, args);
     }
-
 }
