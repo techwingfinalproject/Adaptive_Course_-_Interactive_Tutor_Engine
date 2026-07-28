16e3e92f62f83573d91001493c2b6d365b85a008
seshu7337 <seshutallapu@gmail.com>
2026-07-28 21:54:34 +0530

    Update API URL in AuthService

--- adaptive_course_front_end/src/app/services/auth.service.ts
+++ adaptive_course_front_end/src/app/services/auth.service.ts
@@ -6,7 +6,7 @@ import { Observable, BehaviorSubject, tap } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://13.127.83.187:8080/api'; 
+  private apiUrl = 'http://13.127.83.187:8080/adaptive_course_backend/api'; 
   
   private loggedInSubject = new BehaviorSubject<boolean>(this.isLoggedIn());
   public isLoggedIn$ = this.loggedInSubject.asObservable();
