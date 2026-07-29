4245ee98219b056e2d2199d33d348ef6313c15cc
seshu7337 <seshutallapu@gmail.com>
2026-07-29 12:49:21 +0530

    Change imports from jakarta to javax in Quiz.java
    Updated import statements to use javax package instead of jakarta.

--- adaptive_course_backend/src/main/java/com/example/demo/models/Quiz.java
+++ adaptive_course_backend/src/main/java/com/example/demo/models/Quiz.java
@@ -2,9 +2,9 @@ package com.example.demo.models;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "quizzes")
